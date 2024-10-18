4bda2bfdecb1c46a283f2908103ce02ca3dfe303
palvevaishnav <palvevaishnav09@gmail.com>
2024-10-18 17:32:43 +0530

    adding profile ui and dummy ui for dashboard

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -1,9 +1,5 @@
 "use client"
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { ProviderUser } from '@/state/user';
-import { UserAtom } from '@/state/user'; // Import the new user atom
+import React from 'react';
 import RightPanel from '@/components/Rightpanel';
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -14,34 +10,7 @@ export default function Layout({
     children: JSX.Element
 }) {
     const { data: session, status } = useSession();
-    const router = useRouter();
-    const user = useRecoilValue(ProviderUser);
-    const setUser = useSetRecoilState(UserAtom); // Create a setter for the user atom
-
-    useEffect(() => {
-        // Send the POST request when the component mounts or when the user state changes
-        const sendUserData = async () => {
-            try {
-                const response = await axios.post(`${process.env.BACKEND_URL}/api/getuser`, user, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-
-                console.log('User data sent successfully:', response.data);
-
-                // Store the user data in the user atom
-                setUser(response.data.user);
-            } catch (error) {
-                console.error('Failed to send user data:', error);
-            }
-        };
-
-        // Only send the request if the user data is populated
-        if (user.providerId) {
-            sendUserData();
-        }
-    }, [user, setUser]); // Re-run the effect if the user state changes
+    const router = useRouter()
     if (status === "loading") {
         return <p>Loading...</p>;
     }
